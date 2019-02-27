ad27dfc2b84a3f8483d35128dc0c3ee132ea5152
Abby Gottlich <abby.gottlich@gmail.com>
2019-02-26 19:11:59 -0600

    all math functions written, imported, and working

--- src/index.js
+++ src/index.js
@@ -1,22 +1,25 @@
 import add from "./modules/add.js";
 import subtract from "./modules/subtract.js";
 import multiply from "./modules/multiply.js";
-import {addTen, addFive} from "./modules/addNumbers.js";
-import {subtractOne} from "./modules/subtractNumbers.js";
+import divide from "./modules/divide.js";
+import { addTen, addFive, addOne } from "./modules/addNumbers.js";
+import { subtractOne, subtractFive, subtractTen } from "./modules/subtractNumbers.js";
+import { multiplyOne, multiplyFive, multiplyTen } from "./modules/multiplyNumbers.js";
+import { divideOne, divideFive, divideTen } from "./modules/divideNumbers.js";
 
 const number1 = 393;
 const number2 = 238;
 
-let answer = add(number1,number2);
+let answer = add(number1, number2);
 console.log(answer);
 
-answer = subtract(number1,number2);
+answer = subtract(number1, number2);
 console.log(answer);
 
-answer = multiply(number1,number2);
+answer = multiply(number1, number2);
 console.log(answer);
 
-answer = divide(number1,number2);
+answer = divide(number1, number2);
 console.log(answer);
 
 answer = addTen(number1);
@@ -35,4 +38,22 @@ answer = subtractFive(number1);
 console.log(answer);
 
 answer = subtractTen(number1);
+console.log(answer);
+
+answer = multiplyOne(number1);
+console.log(answer);
+
+answer = multiplyFive(number1);
+console.log(answer);
+
+answer = multiplyTen(number1);
+console.log(answer);
+
+answer = divideOne(number1);
+console.log(answer);
+
+answer = divideFive(number1);
+console.log(answer);
+
+answer = divideTen(number1);
 console.log(answer);
---
+++ src/modules/multiplyNumbers.js
@@ -0,0 +1,13 @@
+import multiply from "./multiply.js";
+
+export function multiplyTen(num) {
+    return multiply(num, 10);
+}
+
+export function multiplyFive(num) {
+    return multiply(num, 5);
+}
+
+export function multiplyOne(num) {
+    return multiply(num, 1);
+}
